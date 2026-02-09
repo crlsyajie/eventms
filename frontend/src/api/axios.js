f847553743221abdf0456c7ea2bbf44b24f3e45b
crlsyajie <cyfetizanan@gmail.com>
2026-02-09 15:32:36 +0800

    adding the set pending

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,23 @@
 import axios from "axios";
+import { getCSRFToken } from "./csrf";
 
 const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api", // Django backend
   withCredentials: true,
 });
+
+// Add CSRF token to requests
+api.interceptors.request.use(
+  (config) => {
+    const token = getCSRFToken();
+    if (token) {
+      config.headers['X-CSRFToken'] = token;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default api;
